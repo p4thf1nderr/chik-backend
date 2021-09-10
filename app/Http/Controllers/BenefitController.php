d96eca6a9098b0cd018c021fd95f1ce322b2c6e6
artem.tretyakov <artem.tretyakov.91@inbox.ru>
2021-09-10 20:00:53 +0300

    апишечка пошла

---
+++ app/Http/Controllers/BenefitController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\benefit;
+use Illuminate\Http\Request;
+
+class BenefitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\benefit  $benefit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(benefit $benefit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\benefit  $benefit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(benefit $benefit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\benefit  $benefit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, benefit $benefit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\benefit  $benefit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(benefit $benefit)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/CourierController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\courier;
+use Illuminate\Http\Request;
+
+class CourierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\courier  $courier
+     * @return \Illuminate\Http\Response
+     */
+    public function show(courier $courier)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\courier  $courier
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(courier $courier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\courier  $courier
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, courier $courier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\courier  $courier
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(courier $courier)
+    {
+        //
+    }
+}
